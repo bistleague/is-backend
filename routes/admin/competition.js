d989dc94c42a05dde8adeceae5d057901b6a28d2
didithilmy <didithilmy@gmail.com>
2019-08-29 12:51:51 +0700

    Add teams list

--- routes/admin/competition.js
+++ routes/admin/competition.js
@@ -7,6 +7,7 @@ const { CompetitionStage } = require("../../model/CompetitionStage");
 const { getConfig } = require("../../datastore/config");
 const usersRepository = require('../../datastore/users');
 const teamsRepository = require('../../datastore/team');
+const filesRepository = require('../../datastore/files');
 
 
 module.exports = function (fastify, opts, next) {
@@ -20,10 +21,41 @@ module.exports = function (fastify, opts, next) {
 
     fastify.get('/teams', async (req, reply) => {
         const teams = await teamsRepository.getAllTeams();
-        return teams;
-    });
 
-    require('./competition/team')(fastify);
+        let processedTeams = [];
+        for (let i = 0; i < teams.length; i++) {
+            const team = teams[i];
+
+            const preliminaryFileId = team.preliminary_submission_file_id;
+            let preliminaryData;
+            if(preliminaryFileId) {
+                const preliminaryFile = await filesRepository.get(preliminaryFileId);
+                preliminaryData = {
+                    file_url: preliminaryFile.url,
+                    file_name: preliminaryFile.filename,
+                    submitted_at: team.preliminary_submission_last_submitted
+                };
+            }
+
+            const semifinalFileId = team.semifinal_submission_file_id;
+            let semifinalData;
+            if(semifinalFileId) {
+                const semifinalFile = await filesRepository.get(semifinalFileId);
+                semifinalData = {
+                    file_url: semifinalFile.url,
+                    file_name: semifinalFile.filename.split('/').pop(),
+                    submitted_at: team.semifinal_submission_last_submitted
+                };
+            }
+
+            processedTeams.push({
+                ...team,
+                preliminary: preliminaryData,
+                semifinal: semifinalData
+            });
+        }
+        return processedTeams;
+    });
 
     next();
 };
